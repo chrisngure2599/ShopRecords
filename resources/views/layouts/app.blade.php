98b966552652e3fe7e6b73aa071ed775132ba36c
chrisngure2599 <chrisngure2599@gmail.com>
2019-12-02 07:55:32 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,10 +35,11 @@
                 <a class="navbar-brand  " href="{{ url('/home') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                @endguest
-                <a class="navbar-item  " href="{{ url('/users') }}">
+a class="navbar-item  " href="{{ url('/users') }}">
                     User management
                 </a>
+                @endguest
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
